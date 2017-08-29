d2da3b6e718d5027293f9aae469eacf738ff633b
Joseph Gordy <jgordy2424@gmail.com>
2017-08-29 12:12:43 -0400

    Finished with SELECT statements and INSERTS and schema

--- todos2.sql
+++ todos2.sql
@@ -21,17 +21,33 @@ INSERT INTO todo (title, details, priority, created_at, completed_at)
            ('Share a Coke', 'Get Coke from vending machine, Notice what name is on the bottle, Find person with said name, Share your Coke with said person', 4,CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0)),
            ('Play Catan', 'Set up game, Find other players, realize you dont have enough time on lunch to play, put away game, scowl at the clock', 2,'2017-05-29 12:00:00', null),
            ('Play the Nope card', 'Hear direction or command that you do not like, take Nope card out of pocket, slam it down on table, dont do command', 1,CURRENT_TIMESTAMP(0), null),
-           ('Wash clothes', 'Put your dirty clothes in the washing machine, throw in detergent pod, close door and turn on washing machine', 5,CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0)),
+           ('Wash clothes', 'Put your dirty clothes in the washing machine, throw in detergent pod, close door and turn on washing machine', 5,'2017-05-29 12:00:00', CURRENT_TIMESTAMP(0)),
            ('Selfie with a stranger', 'Find an unsuspecting person, open camera app, take selfie with stranger', 2,CURRENT_TIMESTAMP(0), null),
            ('Finish Github page', 'Make changes to website, commit and push to Github', 10,CURRENT_TIMESTAMP(0), null),
-           ('Change timestamps on this project', 'Read the instructions wrong, start project, read instructions again, restart project', 10,CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0)),
+           ('Change timestamps on this project', 'Read the instructions wrong, start project, read instructions again, restart project', 10,'2017-05-29 12:00:00', CURRENT_TIMESTAMP(0)),
            ('Buy Iron Yard Swag', 'Go to the Iron Yard site, purchase goodies from them', 3,CURRENT_TIMESTAMP(0), null),
-           ('Take a road trip', 'Agree on a place to go, load up the car, Head to agreed upon place', 9,CURRENT_TIMESTAMP(0), null),
+           ('Take a road trip', 'Agree on a place to go, load up the car, Head to agreed upon place', 9,'2017-05-29 12:00:00', null),
            ('Put Iron Yard sticker on my Mac', 'First buy a sticker, put it on my Mac in my desired location', 4,CURRENT_TIMESTAMP(0), null),
            ('Preorder the new Xbox One X', 'Save money for months because you are on a student budget, buy Xbox 6 months after release because you are on a student budget', 2,CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0));
 
 --  select all incomplete todos with a priority of 3
 SELECT title FROM todo WHERE completed_at IS NULL AND priority = 3;
 
--- select all incomplete todos and sort by priority
-SELECT title, priority, completed_at FROM todo WHERE completed_at IS NULL ORDER BY priority DESC;
+-- get a count of all incomplete todos and grouped by priority
+SELECT COUNT(id), priority FROM todo WHERE completed_at IS NULL GROUP BY priority;
+
+-- find the NUMBER of todos by priority created in the last 30 days
+SELECT COUNT(id), priority, created_at FROM todo WHERE created_at > (current_date - INTERVAL '30') GROUP BY created_at, priority;
+
+-- Find the next todo by finding the oldest created todo with the highest priority
+SELECT title, priority, created_at FROM todo WHERE completed_at IS NULL ORDER BY  created_at ASC, priority DESC;
+
+
+
+
+
+
+
+
+
+--
